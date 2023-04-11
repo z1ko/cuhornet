45e638788b20958cfb6842e5f247cb89e8ea9d39
Z1ko <filippo.ziche@gmail.com>
2023-04-11 14:05:16 +0200

    fix: Top down BFS is now deterministic

--- hornetsnest/include/Static/BreadthFirstSearch/TopDown2.cuh
+++ hornetsnest/include/Static/BreadthFirstSearch/TopDown2.cuh
@@ -47,42 +47,44 @@ namespace hornets_nest {
 using vid_t = int;
 using dist_t = int;
 
-using HornetInit  = ::hornet::HornetInit<vid_t>;
+using HornetInit = ::hornet::HornetInit<vid_t>;
 using HornetDynamicGraph = ::hornet::gpu::Hornet<vid_t>;
 using HornetStaticGraph = ::hornet::gpu::HornetStatic<vid_t>;
 
-//using HornetGraph = gpu::Csr<EMPTY, EMPTY>;
-//using HornetGraph = gpu::Hornet<EMPTY, EMPTY>;
+// using HornetGraph = gpu::Csr<EMPTY, EMPTY>;
+// using HornetGraph = gpu::Hornet<EMPTY, EMPTY>;
 
 using dist_t = int;
 
 template <typename HornetGraph>
 class BfsTopDown2 : public StaticAlgorithm<HornetGraph> {
 public:
-    BfsTopDown2(HornetGraph& hornet);
-    ~BfsTopDown2();
+  BfsTopDown2(HornetGraph &hornet);
+  ~BfsTopDown2();
 
-    void reset()    override;
-    void run()      override;
-    void release()  override;
-    bool validate() override;
+  void reset() override;
+  void run() override;
+  void release() override;
+  bool validate() override;
 
-    void set_parameters(vid_t source);
+  void runAlg(int alg = 0);
 
-    dist_t getLevels(){return current_level;}
+  void set_parameters(vid_t source, int load_balancing = 0);
+
+  dist_t getLevels() { return current_level; }
 
 private:
-    BufferPool pool;
-    TwoLevelQueue<vid_t>        queue;
-    load_balancing::BinarySearch load_balancing;
-    //load_balancing::VertexBased1 load_balancing;
-    dist_t* d_distances   { nullptr };
-    vid_t   bfs_source    { 0 };
-    dist_t  current_level { 0 };
+  BufferPool pool;
+  TwoLevelQueue<vid_t> queue;
+  load_balancing::LogarthimRadixBinning32 load_balancing;
+
+  dist_t *d_distances{nullptr};
+  vid_t bfs_source{0};
+  dist_t current_level{0};
 };
 
 using BfsTopDown2Dynamic = BfsTopDown2<HornetDynamicGraph>;
-using BfsTopDown2Static  = BfsTopDown2<HornetStaticGraph>;
+using BfsTopDown2Static = BfsTopDown2<HornetStaticGraph>;
 
 } // namespace hornets_nest
 
@@ -95,17 +97,18 @@ const dist_t INF = std::numeric_limits<dist_t>::max();
 // OPERATORS //
 ///////////////
 
+struct BFSOperatorAtomic { // deterministic
+  dist_t current_level;
+  dist_t *d_distances;
+  TwoLevelQueue<vid_t> queue;
 
-struct BFSOperatorAtomic {                  //deterministic
-    dist_t               current_level;
-    dist_t*              d_distances;
-    TwoLevelQueue<vid_t> queue;
-
-    OPERATOR(Vertex& vertex, Edge& edge) {
-        auto dst = edge.dst_id();
-        if (atomicCAS(d_distances + dst, INF, current_level) == INF)
-            queue.insert(dst);
+  OPERATOR(Vertex &vertex, Edge &edge) {
+    auto dst = edge.dst_id();
+    if (d_distances[dst] == INF) {
+      if (atomicCAS(d_distances + dst, INF, current_level) == INF)
+        queue.insert(dst);
     }
+  }
 };
 //------------------------------------------------------------------------------
 /////////////////
@@ -115,59 +118,48 @@ struct BFSOperatorAtomic {                  //deterministic
 #define BFSTOPDOWN2 BfsTopDown2<HornetGraph>
 
 template <typename HornetGraph>
-BFSTOPDOWN2::BfsTopDown2(HornetGraph& hornet) :
-                                 StaticAlgorithm<HornetGraph>(hornet),
-                                 queue(hornet, 5),
-                                 load_balancing(hornet) {
-    pool.allocate(&d_distances, hornet.nV());
-    reset();
+BFSTOPDOWN2::BfsTopDown2(HornetGraph &hornet)
+    : StaticAlgorithm<HornetGraph>(hornet), queue(hornet, 5), load_balancing(hornet) {
+  pool.allocate(&d_distances, hornet.nV());
+  reset();
+}
+
+template <typename HornetGraph> BFSTOPDOWN2::~BfsTopDown2() {}
+
+template <typename HornetGraph> void BFSTOPDOWN2::reset() {
+  current_level = 1;
+  queue.clear();
+
+  auto distances = d_distances;
+  forAllnumV(StaticAlgorithm<HornetGraph>::hornet,
+             [=] __device__(int i) { distances[i] = INF; });
 }
 
 template <typename HornetGraph>
-BFSTOPDOWN2::~BfsTopDown2() {
+void BFSTOPDOWN2::set_parameters(vid_t source, int load_balancing) {
+  bfs_source = source;
+  queue.insert(bfs_source); // insert bfs source in the frontier
+  gpu::memsetZero(d_distances + bfs_source); // reset source distance
 }
 
-template <typename HornetGraph>
-void BFSTOPDOWN2::reset() {
-    current_level = 1;
-    queue.clear();
+template <typename HornetGraph> void BFSTOPDOWN2::run() {
+  printf("bfs_source = %d\n", bfs_source);
+  while (queue.size() > 0) {
 
-    auto distances = d_distances;
+    forAllEdges(StaticAlgorithm<HornetGraph>::hornet, queue,
+                BFSOperatorAtomic{current_level, d_distances, queue},
+                load_balancing);
 
-    forAllnumV(
-        StaticAlgorithm<HornetGraph>::hornet,
-        [=] __device__ (int i){ distances[i] = INF; } );
+    queue.swap();
+    current_level++;
+  }
+  // std::cout << "Number of levels is : " << current_level << std::endl;
 }
 
-template <typename HornetGraph>
-void BFSTOPDOWN2::set_parameters(vid_t source) {
-    bfs_source = source;
-    queue.insert(bfs_source);               // insert bfs source in the frontier
-    gpu::memsetZero(d_distances + bfs_source);  //reset source distance
+template <typename HornetGraph> void BFSTOPDOWN2::release() {
+  d_distances = nullptr;
 }
 
-template <typename HornetGraph>
-void BFSTOPDOWN2::run() {
-    while (queue.size() > 0) {
-
-        forAllEdges(
-            StaticAlgorithm<HornetGraph>::hornet,
-            queue,
-                    BFSOperatorAtomic { current_level, d_distances, queue },
-                    load_balancing);
-        queue.swap();
-        current_level++;
-    }
-}
-
-template <typename HornetGraph>
-void BFSTOPDOWN2::release() {
-    d_distances = nullptr;
-}
-
-template <typename HornetGraph>
-bool BFSTOPDOWN2::validate() {
-    return true;
-}
+template <typename HornetGraph> bool BFSTOPDOWN2::validate() { return true; }
 
 } // namespace hornets_nest
